1f320a10427ae9e9cf967c770fb1e468bdf84d2b
motattack <malrixinc@gmail.com>
2023-04-09 13:04:22 +1000

    Add: Drawing a colored triangle

--- main.cpp
+++ main.cpp
@@ -23,6 +23,81 @@ int main() {
         return -1;
     }
 
+    const char *vshader = "#version 330 core\n"
+                          "layout (location = 0) in vec3 inPos;\n"
+                          "void main() {\n"
+                          "gl_Position = vec4(inPos, 1.0);"
+                          "}\n";
+    const char *fshader = "#version 330 core\n"
+                          "out vec4 OutColor;\n"
+                          "void main() {\n"
+                          "OutColor = vec4(0.38f, 0.01f, 0.37f, 0.0f);\n"
+                          "}\n";
+
+    int vshaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vshaderId, 1, &vshader, nullptr);
+    glCompileShader(vshaderId);
+
+    int result = 0;
+    glGetShaderiv(vshaderId, GL_COMPILE_STATUS, &result);
+    if (!result) {
+        char log[512];
+        glGetShaderInfoLog(vshaderId, 512, nullptr, log);
+    }
+
+    int fshaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fshaderId, 1, &fshader, nullptr);
+    glCompileShader(fshaderId);
+
+    glGetShaderiv(fshaderId, GL_COMPILE_STATUS, &result);
+    if (!result) {
+        char log[512];
+        glGetShaderInfoLog(fshaderId, 512, nullptr, log);
+    }
+
+    int shaderId = glCreateProgram();
+    glAttachShader(shaderId, vshaderId);
+    glAttachShader(shaderId, fshaderId);
+    glLinkProgram(shaderId);
+
+    glGetShaderiv(shaderId, GL_LINK_STATUS, &result);
+    if (!result) {
+        char log[512];
+        glGetShaderInfoLog(shaderId, 512, nullptr, log);
+    }
+
+    glDeleteShader(vshaderId);
+    glDeleteShader(fshaderId);
+
+    float verts[]{
+            0.0f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+    };
+
+    unsigned index[] = {
+            0, 1, 2
+    };
+
+    unsigned VA0 = 0, VBA = 0, IBA = 0;
+    glCreateVertexArrays(1, &VA0);
+    glGenBuffers(1, &VBA);
+    glGenBuffers(1, &IBA);
+
+    glBindVertexArray(VA0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBA);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBA);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+
+
     bool isGo = true;
 
     while (isGo) {
@@ -40,6 +115,13 @@ int main() {
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderId);
+        glBindVertexArray(VA0);
+
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+        glBindVertexArray(0);
 
         window.display();
     }
